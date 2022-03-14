cf29a37f14e3578f8690666a010375a067f5bfef
Melody Zhu <sc21myqz@leeds.ac.uk>
2022-03-14 17:47:16 +0000

    write structures.h

--- week2/pgmStructures.h
+++ week2/pgmStructures.h
@@ -1,3 +1,10 @@
-typedef struct _pgmFile {
 
+typedef struct _pgmFile {
+    unsigned int width;
+    unsigned int height;
+    unsigned int gray;
+    unsigned char *imageData;
+    unsigned char magic_number[2];
+    unsigned short *magic_Number;
+    char *commentLine;
 } pgmFile;
