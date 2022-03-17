17d2021b8ca794d7c39c3e6129e0ad00f8e37576
Melody Zhu [sc21myqz] <sc21myqz@uol-pc-051667.leeds.ac.uk>
2022-03-17 13:18:01 +0000

    write tests for pgmEcho.c

--- testscript.sh
+++ testscript.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
 # Testing pgmEcho.c without any file names
+echo "Testing pgmEcho.c without any file names: ./pgmEcho"
+./pgmEcho
+echo ""
 
 # Testing pgmEcho.c with one file name 
+echo "Testing pgmEcho.c with one file name : ./pgmEcho slice0a.pgm"
+./pgmEcho slice0a.pgm
+echo ""
 
 # Testing pgmEcho.c with two file names
+echo "Testing pgmEcho.c with two file names: ./pgmEcho slice0a.pgm output.pgm"
+./pgmEcho slice0a.pgm output.pgm
+echo ""
 
 # Testing pgmEcho.c with three file names
+echo "Testing pgmEcho.c with three file names: ./pgmEcho slice0a.pgm output.pgm balloons.pgm"
+./pgmEcho slice0a.pgm output.pgm balloons.pgm
+echo ""
 
 # Testing pgmEcho.c with an input file name which does not exist
+echo "Testing pgmEcho.c with an input file name which does not exist: ./pgmEcho notexist.pgm output.pgm"
+./pgmEcho notexist.pgm output.pgm
+echo ""
 
 # Testing pgmComp.c without any file names
 
