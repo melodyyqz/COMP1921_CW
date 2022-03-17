8583e8d424670d05a6360c229c4452ce05a54eb5
Melody Zhu [sc21myqz] <sc21myqz@uol-pc-051667.leeds.ac.uk>
2022-03-17 13:07:57 +0000

    writing plans for test script

--- testscript.sh
+++ testscript.sh
@@ -1,1 +1,31 @@
 #!/bin/bash
+
+# Testing pgmEcho.c without any file names
+
+# Testing pgmEcho.c with one file name 
+
+# Testing pgmEcho.c with two file names
+
+# Testing pgmEcho.c with three file names
+
+# Testing pgmEcho.c with an input file name which does not exist
+
+# Testing pgmComp.c without any file names
+
+# Testing pgmComp.c with one file name 
+
+# Testing pgmComp.c with two file names
+
+# Testing pgmComp.c with three file names
+
+# Testing pgmComp.c with an input file name which does not exist
+
+# Testing pgmComp.c with an output file name which does not exist
+
+# Testing pgma2b.c with an ASCII input file
+
+# Testing pgma2b.c with a binary input file
+
+# Testing pgmb2a.c with a binary input file
+
+# Testing pgmb2a.c with an ASCII input file
