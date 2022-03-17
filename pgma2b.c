acfed647b519e6c3626031a5f73ec635472c6d4e
Melody Zhu [sc21myqz] <sc21myqz@uol-pc-051667.leeds.ac.uk>
2022-03-17 14:26:33 +0000

    make function for a2b so it can be referred to elsewhere

--- pgma2b.c
+++ pgma2b.c
@@ -8,6 +8,17 @@
 #include "definitions.h"
 #include "pgmReadWrite.h"
 
+int a2b(pgmFile *firstPgm, char* inputFileName, char* outputFileName){
+    // makes the second pgm struct identical to the first pgm struct
+    pgmFile *secondPgm = firstPgm;
+
+    // reads in the first file and writes it as binary to the second file
+    if (fileRead(inputFileName, firstPgm)==0 && fileWrite(outputFileName, secondPgm, 5)==0){
+        printf("CONVERTED\n");
+        return 0;
+}
+}
+
 int main(int argc, char **argv){
     // check arguments
     argCheck(argc, 3, argv[0]);
@@ -19,14 +30,6 @@ int main(int argc, char **argv){
     // initialising the first pgm file and initialise the first pgm's struct
     pgmFile *firstPgm = (pgmFile *)malloc(sizeof(pgmFile));
     initialiseStruct(firstPgm);
-
-    // makes the second pgm struct identical to the first pgm struct
-    pgmFile *secondPgm = firstPgm;
-
-    // reads in the first file and writes it as binary to the second file
-    if (fileRead(argv[1], firstPgm)==0 && fileWrite(argv[2], secondPgm, 5)==0){
-        printf("CONVERTED\n");
-        return 0;
-    }
+    a2b(firstPgm, argv[1], argv[2]);
 
 }
